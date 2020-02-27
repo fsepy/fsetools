cb9061d9e55e4a99b91f557653d32e0b49e548b6
fuyans <fuyans@gmail.com>
2020-02-27 15:24:44 +0000

    Implemented custom QMainWindow.

--- fsetools/gui/logic/dialog_0103_bs9999_merging_flow.py
+++ fsetools/gui/logic/dialog_0103_bs9999_merging_flow.py
@@ -1,13 +1,12 @@
 from PySide2 import QtWidgets, QtGui, QtCore
-from fsetools.gui.logic.main_0000_QMainWindow import QMainWindow
 
-from fsetools.gui.images_base64 import dialog_0103_bs9999_merging_flow_context as image_context
 from fsetools.gui.images_base64 import dialog_0103_bs9999_mergine_flow_figure_1 as image_figure_1
 from fsetools.gui.images_base64 import dialog_0103_bs9999_mergine_flow_figure_2 as image_figure_2
 from fsetools.gui.images_base64 import dialog_0103_bs9999_mergine_flow_figure_3 as image_figure_3
-
+from fsetools.gui.images_base64 import dialog_0103_bs9999_merging_flow_context as image_context
 from fsetools.gui.layout.dialog_0103_merging_flow import Ui_MainWindow
 from fsetools.gui.logic.common import filter_objects_by_name
+from fsetools.gui.logic.main_0000_QMainWindow import QMainWindow
 from fsetools.libstd.bs_9999_2017 import (
     clause_15_6_6_e_merging_flow_1, clause_15_6_6_e_merging_flow_2, clause_15_6_6_e_merging_flow_3
 )
