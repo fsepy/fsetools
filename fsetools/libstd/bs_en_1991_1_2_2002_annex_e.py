568130022087d2b8b6c854c3a0a45d900a21b226
fuyans <fuyans@gmail.com>
2020-07-20 22:47:26 +0100

    Added a new test function.

--- fsetools/libstd/bs_en_1991_1_2_2002_annex_e.py
+++ fsetools/libstd/bs_en_1991_1_2_2002_annex_e.py
@@ -1,24 +1,39 @@
 from typing import Union
 
+SYMBOLS = dict(
+    q_fk=("MJ/m**2", "characteristic fire load density related to the floor area to $A_t$"),
+    m=(),
+    delta_q1=(),
+    delta_q2=(),
+    delta_n=(),
+    q_fd=(),
+)
+
 
 def equation_e1_design_fire_load_density(
-        q_f_k,
+        q_fk,
         m,
         delta_q1,
         delta_q2,
         delta_n,
         *_,
         **__
-):
-    q_f_d = q_f_k * m * delta_q1 * delta_q2 * delta_n
-    return q_f_d
+) -> dict:
+    q_fd = q_fk * m * delta_q1 * delta_q2 * delta_n
+
+    _latex = [
+        f'q_{{fd}}=q_{{fk}}\\cdot m\\cdot \\delta_{{q1}}\\cdot \\delta_{{q2}}\\cdot \\delta_n',
+        f'q_{{fd}}={q_fk:.2f}\\cdot {m:.2f}\\cdot \\{delta_q1:.2f}\\cdot \\{delta_q2:.2f}\\cdot \\{delta_n:.2f}',
+        f'q_{{fd}}={q_fd:.2f}',
+    ]
+    return dict(q_fd=q_fd, _latex=_latex)
 
 
 def table_e1_delta_q1(
         A_f,
         *_,
         **__,
-):
+) -> float:
     if A_f <= 25:
         delta_q1 = 1.1
     elif A_f <= 250:
@@ -38,7 +53,7 @@ def table_e1_delta_q2(
         occupancy: Union[str, int],
         *_,
         **__,
-):
+) -> float:
     occupancy = occupancy.lower().strip()
 
     if occupancy == 1 or occupancy in ['artgallery', 'musem', 'swimming pool']:
@@ -61,7 +76,6 @@ def table_e2_delta_n(
         is_sprinklered: bool,
         is_sprinkler_independent_water_supplies: Union[bool, int],
 
-        is_automatic_fire_detection: bool,
         is_detection_by_heat: bool,
         is_detection_by_smoke: bool,
         is_automatic_transmission_to_fire_brigade: bool,
@@ -75,14 +89,16 @@ def table_e2_delta_n(
         *_,
         **__,
 
-):
+) -> float:
     delta_n1 = 0.61 if is_sprinklered else 1
+
     if is_sprinklered and is_sprinkler_independent_water_supplies == 1:
         delta_n2 = 0.87
     elif is_sprinklered and is_sprinkler_independent_water_supplies == 2:
         delta_n2 = 0.7
     else:
         delta_n2 = 1
+
     delta_n3 = 0.87 if is_detection_by_heat else 1
     delta_n4 = 0.73 if is_detection_by_smoke else 1
     delta_n5 = 0.87 if is_automatic_transmission_to_fire_brigade else 1
@@ -97,3 +113,53 @@ def table_e2_delta_n(
         delta_n6 * delta_n7 * delta_n8 * delta_n9 * delta_n10
 
     return delta_n
+
+
+def _test(is_assertion=True):
+    A_f = 500
+    q_fk = 870
+    m = 0.8
+    is_sprinklered = True
+    is_sprinkler_independent_water_supplies = True
+    is_detection_by_heat = False
+    is_detection_by_smoke = True
+    is_automatic_transmission_to_fire_brigade = False
+    is_onsite_fire_brigade = False
+    is_offsite_fire_brigade = True
+    is_safe_access_routes = True
+    is_fire_fighting_devices = True
+    is_smoke_exhaust_system = True
+
+    str_fmt = '{:>10.10}: {:<.2f}'
+
+    delta_q1 = table_e1_delta_q1(A_f=A_f)
+    delta_q2 = table_e1_delta_q2(occupancy='office')
+    delta_n = table_e2_delta_n(
+        is_sprinklered=is_sprinklered,
+        is_sprinkler_independent_water_supplies=is_sprinkler_independent_water_supplies,
+        is_detection_by_heat=is_detection_by_heat,
+        is_detection_by_smoke=is_detection_by_smoke,
+        is_automatic_transmission_to_fire_brigade=is_automatic_transmission_to_fire_brigade,
+        is_onsite_fire_brigade=is_onsite_fire_brigade,
+        is_offsite_fire_brigade=is_offsite_fire_brigade,
+        is_safe_access_routes=is_safe_access_routes,
+        is_fire_fighting_devices=is_fire_fighting_devices,
+        is_smoke_exhaust_system=is_smoke_exhaust_system
+    )
+    print(str_fmt.format('delta', delta_q1 * delta_q2 * delta_n))
+
+    q_fd = equation_e1_design_fire_load_density(
+        q_fk=q_fk,
+        m=m,
+        delta_q1=delta_q1,
+        delta_q2=delta_q2,
+        delta_n=delta_n
+    )['q_fd']
+    print('{:>10.10}: {:<.10f}'.format('q_fd', q_fd))
+
+    if is_assertion:
+        assert abs(q_fd - 399.6035989920) < 1e-8
+
+
+if __name__ == '__main__':
+    _test(False)
