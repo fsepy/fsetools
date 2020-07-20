7c223c7a1e35e7c8f01d69522f90b8f3543c2181
fuyans <fuyans@gmail.com>
2020-07-20 22:46:54 +0100

    New bs_en_1991_1_2_2002_annex_a module currently under development.

--- fsetools/libstd/bs_en_1991_1_2_2002_annex_a.py
+++ fsetools/libstd/bs_en_1991_1_2_2002_annex_a.py
@@ -1,107 +1,33 @@
 import numpy as np
 
 
-def clause_a_3_temperature_time_heating_phase(t_star):
-    # equation A.1, page 30
-    a = 0.324 * np.exp(-0.2 * t_star)
-    b = 0.204 * np.exp(-1.7 * t_star)
-    c = 0.472 * np.exp(-19 * t_star)
-    T_g = 1325 * (1 - a - b - c)
-    return T_g
-
-
-def clause_a_3_t_star(t, Gamma):
-    # equation A.2a, page 30
-    t_star = t * Gamma
-    return t_star
-
-
-def clause_a_7_t_star_max(t_max, Gamma):
-    # equation A.6, page 31
-    t_star_max = t_max * Gamma
-    return t_star_max
-
-
-def clause_a_7_t_max(t_lim, q_t_d, O):
-    # equation A.7, page 31
-    t_max = max([0.0002 * q_t_d / O, t_lim])
-    return t_max
-
-
-def clause_a_8_t_star(q_t_d, t_lim, b, t, t_max):
-
-    assert abs(t_max - t_lim) < 1e-3
-
-    # equation A.9, page 32
-    O_lim = 0.1e-3 * q_t_d / t_lim
-    # equation A.8, page 32
-    Gamma_lim = ((O_lim / b) ** 2) / ((0.04 / 1160) ** 2)
-    # equation A.2b, page 32
-    t_star = t * Gamma_lim
-    return t_star
-
-
-def clause_a_11_temperature_time_curve_cooling_phase(
-        q_t_d,
-        Gamma,
-        O,
-        t_max,
-        t_lim,
-        Theta_max,
-        t_star,
-        override_t_max_lower_than_t_lim: bool = False
+def appendix_a_parametric_fire(
+        t: np.ndarray,
+        A_t: float,
+        A_f: float,
+        A_v: float,
+        h_eq: float,
+        q_fd: float,
+        lambda_: float,
+        rho: float,
+        c: float,
+        t_lim: float,
+        temperature_initial: float = 293.15
 ):
-
-    # equation A.12, page 32
-    t_star_max = (0.2e-3 * q_t_d / O) * Gamma
-    if t_max > t_lim:
-        x = 1
-    elif abs(t_max - t_lim) < 1e-3 or override_t_max_lower_than_t_lim:  # t_max == t_lim
-        x = t_lim * Gamma / t_star_max
-    else:
-        raise ValueError(f'Conditions t_max > t_lim or t_max == t_lim not met, {t_max} {t_lim}')
-
-    if t_star_max <= 0.5:
-        # equation A.11a
-        Theta_g = Theta_max - 625 * (t_star - t_star_max * x)
-    elif 0.5 < t_star_max < 2.0:
-        # equation A.11b
-        Theta_g = Theta_max - 250 * (3 - t_star_max) * (t_star - t_star_max * x)
-    elif 2.0 <= t_star_max:
-        # equation A.11c
-        Theta_g = Theta_max - 250 * (t_star - t_star_max * x)
-    else:
-        Theta_g = np.nan
-    return Theta_g
-
-
-def fire(
-        t, A_t, A_f, A_v, h_eq, q_fd, lambda_, rho, c, t_lim, temperature_initial=293.15
-):
-    """
-    Function Description:
-
+    """Function Description: (SI UNITS ONLY)
     This function calculates the time-temperature curve according to Eurocode 1 part 1-2, Appendix A.
-
-    :param t: numpy.ndarray, [s], time evolution.
-    :param A_t: float, [m2], total surface area (including openings).
-    :param A_f: float, [m2], floor area.
-    :param A_v: float, [m2], opening area.
-    :param h_eq: float, [m2], opening height.
-    :param q_fd: float, [J/m2], fuel density.
-    :param lambda_: float, [K/kg/m], lining thermal conductivity.
-    :param rho: float, [kg/m3], lining density.
-    :param c: float, [J/K/kg], lining thermal capacity.
-    :param t_lim: float, [s], limiting time for the fire.
+    :param t: [s], time evolution.
+    :param A_t: [m2], total surface area (including openings).
+    :param A_f: [m2], floor area.
+    :param A_v: [m2], opening area.
+    :param h_eq: [m2], opening height.
+    :param q_fd: [J/m2], fuel density.
+    :param lambda_: [K/kg/m], lining thermal conductivity.
+    :param rho: [kg/m3], lining density.
+    :param c: [J/K/kg], lining thermal capacity.
+    :param t_lim: [s], limiting time for the fire.
+    :param temperature_initial: [K], initial (ambient temperature)
     :return T_g: numpy.ndarray, [s], temperature evolution.
-
-
-    Table F.2 - Conversion factor k_b depending on the thermal properties of the enclosure
-    | b=√(ρcλ) [J/m²/s0.5/K]                  | k_b [min⋅m²/MJ]   |
-    |:----------------------------------------|:------------------|
-    | b>2500                                  | 0.04              |
-    | 2500>=b>=720                            | 0.055             |
-    | b<720                                   | 0.07              |
     """
     # Reference: Eurocode 1991-1-2; Jean-Marc Franssen, Paulo Vila Real (2010) - Fire Design of Steel Structures
 
@@ -122,7 +48,7 @@ def fire(
     t_max = 0.0002 * q_td / O
 
     # check criteria
-    # todo: raise warnings to higher level
+    # todo: raise warnings
     # if not 50 <= q_td <= 1000:
     #     if is_cap_q_td:
     #         msg = "q_td ({:4.1f}) EXCEEDED [50, 1000] AND IS CAPPED.".format(q_td, is_cap_q_td)
@@ -131,6 +57,43 @@ def fire(
     #     warnings.warn(msg)
 
     # CALCULATION
+    def _temperature_heating(t_star, temperature_initial):
+        # eq. 3.12
+        T_g = 1325 * (
+                1
+                - 0.324 * np.exp(-0.2 * t_star)
+                - 0.204 * np.exp(-1.7 * t_star)
+                - 0.472 * np.exp(-19 * t_star)
+        )
+        T_g += temperature_initial
+        return T_g
+
+    def _temperature_cooling_vent(t_star_max, T_max, t_star):  # ventilation controlled
+        # eq. 3.16
+        if t_star_max <= 0.5:
+            T_g = T_max - 625 * (t_star - t_star_max)
+        elif 0.5 < t_star_max < 2.0:
+            T_g = T_max - 250 * (3 - t_star_max) * (t_star - t_star_max)
+        elif 2.0 <= t_star_max:
+            T_g = T_max - 250 * (t_star - t_star_max)
+        else:
+            T_g = np.nan
+        return T_g
+
+    def _temperature_cooling_fuel(
+            t_star_max, T_max, t_star, Gamma, t_lim
+    ):  # fuel controlled
+        # eq. 3.22
+        if t_star_max <= 0.5:
+            T_g = T_max - 625 * (t_star - Gamma * t_lim)
+        elif 0.5 < t_star_max < 2.0:
+            T_g = T_max - 250 * (3 - t_star_max) * (t_star - Gamma * t_lim)
+        elif 2.0 <= t_star_max:
+            T_g = T_max - 250 * (t_star - Gamma * t_lim)
+        else:
+            T_g = np.nan
+        return T_g
+
     def _variables(t, Gamma, t_max):
         t_star = Gamma * t
         t_star_max = Gamma * t_max
@@ -149,31 +112,18 @@ def fire(
         return t_star_, t_star_max_
 
     t_star, t_star_max = _variables(t, Gamma, t_max)
-    # t_max = clause_a_7_t_max(t_lim, q_t_d=q_td, O=O)
-    # t_star_max = clause_a_7_t_star_max(t_max=t_max, Gamma=Gamma)
 
-    if t_max >= t_lim:  # ventilation controlled fire
-        T_max = clause_a_3_temperature_time_heating_phase(t_star_max)
-        T_heating_g = clause_a_3_temperature_time_heating_phase(Gamma * t)
-        # T_cooling_g = _temperature_cooling_vent(t_star_max, T_max, t_star)
+    if t_max >= t_lim:  # ventilation controlled appendix_a_parametric_fire
+        T_max = _temperature_heating(t_star_max, temperature_initial)
+        T_heating_g = _temperature_heating(Gamma * t, temperature_initial)
+        T_cooling_g = _temperature_cooling_vent(t_star_max, T_max, t_star)
         fire_type = "ventilation controlled"
-    else:  # fuel controlled fire
+    else:  # fuel controlled appendix_a_parametric_fire
         t_star_, t_star_max_ = _variables_2(t, t_lim, q_td, b, O)
-        T_max = clause_a_3_temperature_time_heating_phase(t_star_max_)
-        T_heating_g = clause_a_3_temperature_time_heating_phase(t_star_)
-        # T_cooling_g = _temperature_cooling_fuel(t_star_max, T_max, t_star, Gamma, t_lim)
+        T_max = _temperature_heating(t_star_max_, temperature_initial)
+        T_heating_g = _temperature_heating(t_star_, temperature_initial)
+        T_cooling_g = _temperature_cooling_fuel(t_star_max, T_max, t_star, Gamma, t_lim)
         fire_type = "fuel controlled"
-
-    T_cooling_g = clause_a_11_temperature_time_curve_cooling_phase(
-        q_t_d=q_td,
-        Gamma=Gamma,
-        O=O,
-        t_max=t_max,
-        t_lim=t_lim,
-        Theta_max=T_max,
-        t_star=t_star,
-        override_t_max_lower_than_t_lim=True
-    )
 
     T_g = np.minimum(T_heating_g, T_cooling_g)
     T_g[T_g < temperature_initial] = temperature_initial
@@ -185,21 +135,96 @@ def fire(
     return T_g
 
 
-def _test_fire():
-    """This is a test function to `fire` within this module `fire_parametric_ec`, it compares the function against
+def example_plot_interflam():
+    try:
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+    except:
+        raise ImportError('Missing library `seaborn` and `matplotlib`')
+    sns.set_style("ticks")
+    sns.set_context(context="paper")
+
+    fig, ax = plt.subplots(figsize=(3.94, 2.76))
+    ax.set_xlabel("Time [minute]")
+    ax.set_ylabel(r"Temperature [$^{\circ}C$]")
+
+    # define geometry
+    w = 16
+    l = 32
+    h = 3
+
+    h_v = 2
+
+    oo = (0.02, 0.04, 0.06, 0.10, 0.14, 0.20)  # desired opening factor
+
+    def opening_factor_2_window_width(of, w, l, h, h_v):
+        # opening factor = Av * sqrt(hv) / At
+        # Av = h_v * w_v
+        # of * At / sqrt(h_v) = h_v * w_v
+        # w_v = of * At / sqrt(h_v) / h_v
+        return of * (2 * (w * l + l * h + h * w)) / h_v ** 0.5 / h_v
+
+    w_v_ = [opening_factor_2_window_width(o, w, l, h, h_v) for o in oo]
+
+    print(w_v_)
+
+    # Calculate appendix_a_parametric_fire curves
+    for w_v in w_v_:
+        x = np.arange(0, 5 * 60 * 60, 1)
+        y = appendix_a_parametric_fire(
+            t=x,
+            A_v=h_v * w_v,
+            A_t=2 * (w * h + h * l + l * w),
+            A_f=w * l,
+            h_eq=h_v,
+            q_fd=600e6,
+            lambda_=1,
+            rho=1,
+            c=720 ** 2,
+            t_lim=20 * 60,
+            temperature_initial=293.15,
+        )
+        ax.plot(
+            x / 60,
+            y - 273.15,
+            label="Opening Factor {:.2f}".format(
+                (h_v * w_v) * h_v ** 0.5 / (2 * (w * h + h * l + l * w))
+            ),
+        )
+
+    ax.legend().set_visible(True)
+    ax.grid(color="k", linestyle="--")
+    ax.set_ylim((0, 1400))
+    ax.set_xlim((0, 300))
+    plt.tight_layout()
+
+    plt.savefig(fname="appendix_a_parametric_fire-ec.png", dpi=300)
+    plt.show()
+
+
+def _test_appendix_a_parametric_fire():
+    """This is a test function to `appendix_a_parametric_fire` within this module `fire_parametric_ec`, it compares the function against
     Figure 7 in Holicky, M. et al [1].
     yan fu, 1 oct 2018
 
     REFERENCES
     [1] Holicky, M., Meterna, A., Sedlacek, G. and Schleich, J.B., 2005. Implementation of eurocodes, handbook 5, design
-    of buildings for the fire situation. Leonardo da Vinci Pilot Project: Luxembourg."""
-
+    of buildings for the appendix_a_parametric_fire situation. Leonardo da Vinci Pilot Project: Luxembourg."""
     import copy
-    import pandas
+    from io import StringIO
+
+    try:
+        from scipy.interpolate import interp1d
+        from scipy import stats
+    except ImportError:
+        raise ImportError('Missing library `scipy`')
+    try:
+        import pandas
+    except ImportError:
+        raise ImportError('Missing library `pandas`')
 
     # LOAD VERIFICATION DATA
-    # data are from the referenced document
-    from io import StringIO
+    # data are extracted from the referenced document
 
     verification_data = StringIO(
         """time_1,temperature_1,time_2,temperature_2,time_3,temperature_3,time_4,temperature_4,time_5,temperature_5,time_6,temperature_6,time_7,temperature_7
@@ -234,11 +259,11 @@ def _test_fire():
     # define opening area
     A_v_list = [72, 50.4, 36.000000001, 32.4, 21.6, 14.4, 7.2]
 
-    # calculate fire curves
+    # calculate appendix_a_parametric_fire curves
     x1_list = []  # calculated time array
     y1_list = []  # calculated temperature array
     for i in A_v_list:
-        y = fire(A_v=i, **copy.copy(kws))
+        y = appendix_a_parametric_fire(A_v=i, **copy.copy(kws))
         x = np.arange(0, 2 * 60 * 60 + 1, 1) + 10 * 60
         x1_list.append(x / 60)
         y1_list.append(y - 273.15)
@@ -246,8 +271,6 @@ def _test_fire():
     # COMPARE CALCULATED AND THE VERIFICATION DATA
 
     def r_square(x1, y1, x2, y2):
-        from scipy.interpolate import interp1d
-        from scipy import stats
 
         slope, intercept, r_value, p_value, std_err = stats.linregress(
             y2, interp1d(x1, y1)(x2)
@@ -264,4 +287,4 @@ def _test_fire():
 
 if __name__ == "__main__":
     # exmaple_plot_interflam()
-    _test_fire()
+    _test_appendix_a_parametric_fire()
