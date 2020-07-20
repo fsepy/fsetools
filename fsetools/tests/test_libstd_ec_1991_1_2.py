0fdc7e95126b0f6aaa62b095f7bbbf091b9b0f5d
fuyans <fuyans@gmail.com>
2020-07-20 22:50:35 +0100

    Update test_libstd_ec_1991_1_2.py

--- fsetools/tests/test_libstd_ec_1991_1_2.py
+++ fsetools/tests/test_libstd_ec_1991_1_2.py
@@ -1,3 +1,3 @@
-from fsetools.libstd.ec_1991_1_2 import _test_appendix_a_parametric_fire as test_appendix_a_parametric_fire
+from fsetools.libstd.bs_en_1991_1_2_2002_annex_a import _test_appendix_a_parametric_fire as test_appendix_a_parametric_fire
 
 test_appendix_a_parametric_fire()
