bc5e390b3f08a12ac2af5d4731fa307cdf7901cd
fuyans <fuyans@gmail.com>
2020-08-31 21:24:00 +0100

    Moved `c_steel_T` out of parent function.

--- fsetools/lib/fse_bs_en_1993_1_2_heat_transfer.py
+++ fsetools/lib/fse_bs_en_1993_1_2_heat_transfer.py
@@ -2,6 +2,24 @@
 import numpy as np
 
 
+def c_steel_T(temperature):
+    # BS EN 1993-1-2:2005, 3.4.1.2
+
+    temperature -= 273.15
+    if temperature < 20:
+        return 425 + 0.773 * 20 - 1.69e-3 * 400 + 2.22e-6 * 8000
+    if 20 <= temperature < 600:
+        return 425 + 0.773 * temperature - 1.69e-3 * 400 + 2.22e-6 * 8000
+    elif 600 <= temperature < 735:
+        return 666 + 13002 / (738 - temperature)
+    elif 735 <= temperature < 900:
+        return 545 + 17820 / (temperature - 731)
+    elif 900 <= temperature <= 1200:
+        return 650
+    elif temperature > 1200:
+        return 650
+
+
 def protected_steel_eurocode(
         fire_time,
         fire_temperature,
@@ -23,7 +41,7 @@ def protected_steel_eurocode(
     . Ambient (fire) time-temperature data must be given, as well as the parameters specified below.
     :param fire_time: ndarray, [s], time evolution.
     :param fire_temperature: ndarray, [K], imposed temperature evolution.
-    :param beam_rho: float, [kg/m3], steel density.
+    :param beam_rho: float, [kg/m3], sstteel density.
     :param beam_cross_section_area: float, [m2], steel cross section area.
     :param protection_k: float, [K/kg/m], protection thermal conductivity.
     :param protection_rho: float, [kg/m3], protection density.
@@ -38,36 +56,6 @@ def protected_steel_eurocode(
     # todo: 4.2.5.2 (2) - thermal properties for the insulation material
     # todo: revise BS EN 1993-1-2:2005, Clauses 4.2.5.2
 
-    # BS EN 1993-1-2:2005, 3.4.1.2
-    # return self.__make_property("c")
-
-    def c_steel_T(temperature):
-
-        temperature -= 273.15
-        if temperature < 20:
-            # warnings.warn('Temperature ({:.1f} °C) is below 20 °C'.format(temperature))
-            return (
-                    425 + 0.773 * 20 - 1.69e-3 * np.power(20, 2) + 2.22e-6 * np.power(20, 3)
-            )
-        if 20 <= temperature < 600:
-            return (
-                    425
-                    + 0.773 * temperature
-                    - 1.69e-3 * np.power(temperature, 2)
-                    + 2.22e-6 * np.power(temperature, 3)
-            )
-        elif 600 <= temperature < 735:
-            return 666 + 13002 / (738 - temperature)
-        elif 735 <= temperature < 900:
-            return 545 + 17820 / (temperature - 731)
-        elif 900 <= temperature <= 1200:
-            return 650
-        elif temperature > 1200:
-            # warnings.warn('Temperature ({:.1f} °C) is greater than 1200 °C'.format(temperature))
-            return 650
-        else:
-            # warnings.warn('Temperature ({:.1f} °C) is outside bound.'.format(temperature))
-            return 0
 
     V = beam_cross_section_area
     rho_a = beam_rho
@@ -82,15 +70,10 @@ def protected_steel_eurocode(
     specific_heat_steel = fire_time * 0.0
 
     # Check time step <= 30 seconds. [BS EN 1993-1-2:2005, Clauses 4.2.5.2 (3)]
-    # time_change = gradient(time)
-    # if np.max(time_change) > 30.:
-    # raise ValueError("Time step needs to be less than 30s: {0}".format(np.max(time)))
 
     flag_heating_started = False
 
-    temperature_steel[0] = fire_temperature[
-        0
-    ]  # initially, steel temperature is equal to ambient
+    temperature_steel[0] = fire_temperature[0]  # initially, steel temperature is equal to ambient
     temperature_ambient_ = iter(fire_temperature)  # skip the first item
     next(temperature_ambient_)  # skip the first item
     for i, T_g in enumerate(temperature_ambient_):
@@ -109,23 +92,15 @@ def protected_steel_eurocode(
         c = (np.exp(phi / 10.0) - 1.0) * (T_g - fire_temperature[i - 1])
         d = fire_time[i] - fire_time[i - 1]
 
-        temperature_rate_steel[i] = (
-                                            a * b * d - c
-                                    ) / d  # deviated from e4.27, converted to rate [s-1]
+        temperature_rate_steel[i] = (a * b * d - c) / d  # deviated from e4.27, converted to rate [s-1]
 
         temperature_steel[i] = temperature_steel[i - 1] + temperature_rate_steel[i] * d
 
-        if (temperature_rate_steel[i] > 0 and flag_heating_started is False) and fire_time[
-            i
-        ] > 1800:
+        if (temperature_rate_steel[i] > 0 and flag_heating_started is False) and fire_time[i] > 1800:
             flag_heating_started = True
 
         # Terminate steel temperature calculation if necessary
-        if (
-                terminate_when_cooling
-                and flag_heating_started
-                and temperature_rate_steel[i] < 0
-        ):
+        if terminate_when_cooling and flag_heating_started and temperature_rate_steel[i] < 0:
             break
         elif flag_heating_started and terminate_max_temperature < temperature_steel[i]:
             break
@@ -136,11 +111,6 @@ def protected_steel_eurocode(
         #       The steel temperature is forced to be increased or remain as previous when ambient temperature and
         #       its previous temperature are all higher than the current calculated temperature.
         #       A better implementation is perhaps to use a 1-D heat transfer model.
-
-        # DEPRECIATED 26 MAR 2019
-        # if temperature_steel[i] < temperature_steel[i-1] or temperature_steel[i] < temperature_ambient[i]:
-        #     temperature_rate_steel[i] = 0
-        #     temperature_steel[i] = temperature_steel[i-1]
 
     return temperature_steel
 
@@ -185,34 +155,6 @@ def protected_steel_eurocode_max_temperature(
     # todo: 4.2.5.2 (2) - thermal properties for the insulation material
     # todo: revise BS EN 1993-1-2:2005, Clauses 4.2.5.2
 
-    def c_steel_T(temperature):
-
-        temperature -= 273.15
-        if temperature < 20:
-            # warnings.warn('Temperature ({:.1f} °C) is below 20 °C'.format(temperature))
-            return (
-                    425 + 0.773 * 20 - 1.69e-3 * np.power(20, 2) + 2.22e-6 * np.power(20, 3)
-            )
-        if 20 <= temperature < 600:
-            return (
-                    425
-                    + 0.773 * temperature
-                    - 1.69e-3 * np.power(temperature, 2)
-                    + 2.22e-6 * np.power(temperature, 3)
-            )
-        elif 600 <= temperature < 735:
-            return 666 + 13002 / (738 - temperature)
-        elif 735 <= temperature < 900:
-            return 545 + 17820 / (temperature - 731)
-        elif 900 <= temperature <= 1200:
-            return 650
-        elif temperature > 1200:
-            # warnings.warn('Temperature ({:.1f} °C) is greater than 1200 °C'.format(temperature))
-            return 650
-        else:
-            # warnings.warn('Temperature ({:.1f} °C) is outside bound.'.format(temperature))
-            return 0
-
     V = beam_cross_section_area
     rho_a = beam_rho
     lambda_p = protection_k
@@ -221,21 +163,9 @@ def protected_steel_eurocode_max_temperature(
     A_p = protection_protected_perimeter
     c_p = protection_c
 
-    # temperature_steel = time * 0.
-    # temperature_rate_steel = time * 0.
-    # specific_heat_steel = time * 0.
-
-    # Check time step <= 30 seconds. [BS EN 1993-1-2:2005, Clauses 4.2.5.2 (3)]
-    # time_change = gradient(time)
-    # if np.max(time_change) > 30.:
-    # raise ValueError("Time step needs to be less than 30s: {0}".format(np.max(time)))
-
     flag_heating_started = False
 
-    # T_ini = temperature_ambient[0]  # temperature at beginning
     T = fire_temperature[0]  # current steel temperature
-    # T_max = -1  # maximum steel temperature
-    # c = c_steel_T(293.15)
     d = fire_time[1] - fire_time[0]
 
     for i in range(1, len(fire_temperature)):
@@ -255,9 +185,6 @@ def protected_steel_eurocode_max_temperature(
 
         T += dT * d
 
-        if not (T >= 0 or T <= 0):
-            print("d")
-
         if not flag_heating_started:
             if fire_time[i] >= terminate_check_wait_time:
                 if dT > 0:
@@ -270,11 +197,10 @@ def protected_steel_eurocode_max_temperature(
             if dT < 0:
                 T -= dT * d
                 break
-    # print(T)
     return T
 
 
-if __name__ == "__main__":
+def _interflam_figures():
     from sfeprapy.func.fire_iso834 import fire
     import numpy as np
 
@@ -315,3 +241,30 @@ if __name__ == "__main__":
     plt.legend(loc=4)
     plt.tight_layout()
     plt.show()
+
+
+def _speed_test():
+    rho = 7850
+    t = np.arange(0, 700, 0.1)
+    T = 345.0 * np.log10(t * 8.0 + 1.0) + 293.15
+
+    list_dp = np.arange(0.0001, 0.01 + 0.002, 0.001)
+
+    for d_p in list_dp:
+        T_s = protected_steel_eurocode(
+            fire_time=t,
+            fire_temperature=T,
+            beam_rho=rho,
+            beam_cross_section_area=0.017,
+            protection_k=0.2,
+            protection_rho=800,
+            protection_c=1700,
+            protection_thickness=d_p,
+            protection_protected_perimeter=2.14,
+        )
+
+
+if __name__ == "__main__":
+    import timeit
+
+    print(timeit.timeit(_speed_test, number=10))
