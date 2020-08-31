1adf53d7092ad6b58464d5548a041abfc4e183d6
fuyans <fuyans@gmail.com>
2020-08-31 22:11:07 +0100

    Delete speed_test.py

--- fsetools/speed_test.py
+++
@@ -1,7 +0,0 @@
-def test():
-    from fsetools.lib.fse_bs_en_1993_1_2_heat_transfer import _speed_test as test1
-    from fsetools.lib.fse_bs_en_1993_1_2_heat_transfer_c import _speed_test as test2
-    from timeit import timeit
-
-    print(timeit(test1, number=2))
-    print(timeit(test2, number=2))
