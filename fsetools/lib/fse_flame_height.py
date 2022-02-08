bcd9287b00b49eebf43a09b9d56c1ae29a300b1a
fuyans <fuyans@gmail.com>
2022-02-08 15:13:49 +0000

    Improved error messages.

--- fsetools/lib/fse_flame_height.py
+++ fsetools/lib/fse_flame_height.py
@@ -5,7 +5,7 @@ def mean_flame_height_pd_7974(
 ):
     """Calculates mean flame height in accordance with Clause 8.3.2 in PD 7974-1:2019
 
-    :param Q_dot_star: dimensionless heat release rate.
+    :param Q_dot_star:  dimensionless heat release rate.
     :param fuel_type:   0   Natural gas 10 cm to 50 cm diameter burner [1]
                         1   10 cm to 200 cm side wood cribs ΔH_c = 18.6 MJ/kg [2]
                         2   Gas, liquids and solids ΔH_c/φ²=3185 kJ/kg of air [3]
@@ -24,17 +24,26 @@ def mean_flame_height_pd_7974(
         elif Q_dot_star < 40:
             flame_height = fire_diameter * 3.3 * Q_dot_star ** (2 / 5)
         else:
-            raise AssertionError(f'Condition 0 < Q_dot_star < 40 not satisfied (Q_dot_star={Q_dot_star:.2f})')
+            raise AssertionError(
+                f'Condition `0 < Q_dot_star < 40` unsatisfied (Q_dot_star={Q_dot_star:.2f}). '
+                f'See Table 1 in PD 7974-1:2019.'
+            )
     elif fuel_type == 1:
         if 0.75 < Q_dot_star < 8.8:
             flame_height = fire_diameter * 3.4 * Q_dot_star ** 0.61
         else:
-            raise AssertionError(f'Condition 0.75 < Q_dot_star < 8.8 not satisfied (Q_dot_star={Q_dot_star:.2f})')
+            raise AssertionError(
+                f'Condition 0.75 < Q_dot_star < 8.8 unsatisfied (Q_dot_star={Q_dot_star:.2f}). '
+                f'See Table 1 in PD 7974-1:2019.'
+            )
     elif fuel_type == 2:
         if 0.12 < Q_dot_star < 12000:
             flame_height = fire_diameter * (3.7 * Q_dot_star ** (2 / 5) - 1.02)
         else:
-            raise AssertionError(f'Condition 0.12 < Q_dot_star < 12000 not satisfied (Q_dot_star={Q_dot_star:.2f})')
+            raise AssertionError(
+                f'Condition 0.12 < Q_dot_star < 12000 unsatisfied (Q_dot_star={Q_dot_star:.2f}). '
+                f'See Table 1 in PD 7974-1:2019.'
+            )
     else:
         raise ValueError('Unknown fuel type.')
 
