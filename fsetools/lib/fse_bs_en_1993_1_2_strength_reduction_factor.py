8d466ed55363bd38694bac01c3d766edaf155763
fuyans <fuyans@gmail.com>
2023-02-03 10:03:40 +0000

    using norm instead of stats

--- fsetools/lib/fse_bs_en_1993_1_2_strength_reduction_factor.py
+++ fsetools/lib/fse_bs_en_1993_1_2_strength_reduction_factor.py
@@ -1,17 +1,15 @@
 from typing import Union
 
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import norm
 
 from fsetools.libstd.bs_en_1993_1_2_2005_k_y_theta import clause_3_2_1_1_k_y_theta as k_y_theta
 
 
 def k_y_theta_prob(theta_a: Union[float, np.ndarray], epsilon_q: Union[float, np.ndarray]):
     k_y_2_T_bar = k_y_theta(theta_a=theta_a)
-
     k_y_2_T_star = (k_y_2_T_bar + 1e-6) / 1.7
-
-    epsilon = stats.norm.ppf(epsilon_q)
+    epsilon = norm.ppf(epsilon_q)
 
     b1 = np.log(k_y_2_T_star / (1 - k_y_2_T_star))
     b2 = 0.412
@@ -21,5 +19,4 @@ def k_y_theta_prob(theta_a: Union[float, np.ndarray], epsilon_q: Union[float, np
     b6 = np.exp(b1 + b2 + b3 + b4 + b5)
 
     k_y_theta_prob_ = (1.7 * b6) / (b6 + 1)
-
     return k_y_theta_prob_
