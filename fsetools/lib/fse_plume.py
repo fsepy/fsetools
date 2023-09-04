7a3716f192ac48408dbec4ec8b7a24c04f1e6302
fuyans <fuyans@gmail.com>
2023-09-04 23:44:10 +0100

    added docstr and updated fire plume diameter calculation

--- fsetools/lib/fse_plume.py
+++ fsetools/lib/fse_plume.py
@@ -1,45 +1,47 @@
-import math
-from typing import Union
+from typing import Union, Optional
 
 import numpy as np
 
+from fsetools.libstd.pd_7974_1_2019 import eq_10_virtual_origin
 
-def flame_region(Q_conv_kW: float, z: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+
+def flame_region(Q_dot_conv_kW: float, z: Union[float, np.ndarray]) -> Union[int, np.ndarray]:
     """
     Calculate z/Q**(2/5) and return flame region.
 
-    :param Q_conv_kW: Power in kW.
+    :param Q_dot_conv_kW: Power in kW.
     :param z: Height in m.
-    :return: Flame region ('flame', 'plume', or 'intermittent').
+    :return: Flame region (1: 'flame', 2: 'intermittent', or 3: 'plume').
     """
-    region_check = z / (Q_conv_kW ** (2 / 5))
+    region_check = z / (Q_dot_conv_kW ** (2 / 5))
 
     if isinstance(z, np.ndarray):
-        regions = np.empty_like(region_check)
-        regions[region_check < 0.08] = 0.
-        regions[(0.08 <= region_check) & (region_check <= 0.2)] = 1.
-        regions[region_check > 0.2] = 2.
+        regions = np.empty_like(region_check, dtype=int)
+        regions[region_check < 0.08] = 0
+        regions[(0.08 <= region_check) & (region_check <= 0.2)] = 1
+        regions[region_check > 0.2] = 2
         return regions
     else:
         if region_check < 0.08:
-            return 0.  # 'flame'
+            return 0  # 'flame'
         elif region_check > 0.2:
-            return 2.  # 'plume'
+            return 2  # 'plume'
         else:
-            return 1.  # 'intermittent'
+            return 1  # 'intermittent'
 
 
-def centre_line_temperature(Q_c_kW: float, z: float, T_0: float) -> (float, float):
+def centre_line_temperature(Q_dot_kW: float, z: float, T_0: float, region: Optional[float] = None) -> (float, float):
     """
-    Calculate centreline temperature rise.
+    Calculate centre-line temperature of a fire plume.
 
-    :param Q_c_kW: Power in kW.
-    :param z: Height in m.
-    :param T_0: Initial temperature.
+    :param Q_dot_kW: kW, total heat release rate.
+    :param z: m, height where the fire plume diameter is measured.
+    :param T_0: K, initial temperature.
+    :param region: 1, Optional, will be calculated if not provided. 0: flame, 1: intermittent and 2: plume
     :return: Tuple of temperature rise and region check value.
     """
-    z_Q_c_factor = z / (Q_c_kW ** (2 / 5))
-    region = flame_region(Q_c_kW, z)
+    z_Q_c_factor = z / (Q_dot_kW ** (2 / 5))
+    region = flame_region(Q_dot_kW, z) if region is None else region
 
     C = 0.9
 
@@ -60,44 +62,35 @@ def centre_line_temperature(Q_c_kW: float, z: float, T_0: float) -> (float, floa
     return delta_T, z_Q_c_factor
 
 
-def compute_fire_dimensions(Q_c_kW: float, HRRPUA_kW_m2: float, conv_frac: float) -> (float, float):
-    """
-    Compute fire area and fire radius.
-
-    :param Q_c_kW: Power in kW.
-    :param HRRPUA_kW_m2: Heat release rate per unit area in kW/sq.m.
-    :param conv_frac: Convective fraction.
-    :return: Tuple of fire area and fire radius.
-    """
-    fire_area = (Q_c_kW / conv_frac) / HRRPUA_kW_m2
-    fire_radius = (fire_area / math.pi) ** 0.5
-    return fire_area, fire_radius
-
-
-def plume_diameter(Q_c_kW: float, HRRPUA_kW_m2: float, conv_frac: float, z_0: float, z: float) -> float:
+def plume_diameter_visible(
+        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float = None, T_0: float = 293.15) -> float:
     """
     Calculate diameter of the plume at a certain height.
+    Equation 51.54 in Chapter 51, SFPE Handbook (2017)
 
-    :param Q_c_kW: Power in kW.
-    :param HRRPUA_kW_m2: Heat release rate per unit area in kW/sq.m.
-    :param conv_frac: Convective fraction.
-    :param z_0: Virtual origin.
+    :param Q_dot_kW: kW, total heat release rate.
+    :param Q_dot_dd_kW_m2: kW/m2, heat release rate per unit area.
+    :param z: m, height where the fire plume diameter is measured.
     :return: Diameter of the plume.
     """
-    _, fire_radius = compute_fire_dimensions(Q_c_kW, HRRPUA_kW_m2, conv_frac)
-    return (fire_radius / (-z_0)) * (z + (z_0 * -1))
+    T_c = centre_line_temperature(Q_dot_kW=Q_dot_kW, z=z, T_0=T_0) if T_c is None else T_c
+    z_0 = eq_10_virtual_origin(Q_dot_kW / Q_dot_dd_kW_m2, Q_dot_kW)
+    d = 0.48 * ((T_c - T_0) ** 0.5) * (z - z_0)
+    return d
 
 
-def virtual_origin(Q_c_kW: float, HRRPUA_kW_m2: float, conv_frac: float) -> float:
+def plume_diameter_at_50_temperature(
+        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float = None, T_0: float = 293.15) -> float:
     """
-    Calculate virtual origin of the fire.
+    Calculate diameter of the plume at a certain height.
+    Equation 13.21 in Chapter 13, SFPE Handbook (2017)
 
-    :param Q_c_kW: Power in kW.
-    :param HRRPUA_kW_m2: Heat release rate per unit area in kW/sq.m.
-    :param conv_frac: Convective fraction.
-    :return: Virtual origin.
+    :param Q_dot_kW: kW, total heat release rate.
+    :param Q_dot_dd_kW_m2: kW/m2, heat release rate per unit area.
+    :param z: m, height where the fire plume diameter is measured.
+    :return: Diameter of the plume.
     """
-    _, fire_radius = compute_fire_dimensions(Q_c_kW, HRRPUA_kW_m2, conv_frac)
-    fire_diameter = 2 * fire_radius
-    z_0 = (-1.02 + (Q_c_kW ** (2 / 5)) / fire_diameter * 0.083) * fire_diameter
-    return z_0 if z_0 < 0. else 0.
+    T_c = centre_line_temperature(Q_dot_kW=Q_dot_kW, z=z, T_0=T_0) if T_c is None else T_c
+    z_0 = eq_10_virtual_origin(Q_dot_kW / Q_dot_dd_kW_m2, Q_dot_kW)
+    d = 0.12 * ((T_c - T_0) ** 0.5) * (z - z_0)
+    return d
