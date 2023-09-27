1b12a27ae7005e1a497d0b9e45172aef7f7c6585
fuyans <fuyans@gmail.com>
2023-09-27 10:01:16 +0100

    refactor and adding visible smoke diameter

--- fsetools/lib/fse_plume.py
+++ fsetools/lib/fse_plume.py
@@ -1,11 +1,11 @@
-from typing import Union, Optional
+from typing import Union
 
 import numpy as np
 
 from fsetools.libstd.pd_7974_1_2019 import eq_10_virtual_origin
 
 
-def flame_region(Q_dot_conv_kW: float, z: Union[float, np.ndarray]) -> Union[int, np.ndarray]:
+def fire_plume_region(Q_dot_conv_kW: float, z: Union[float, np.ndarray]) -> Union[int, np.ndarray]:
     """
     Calculate z/Q**(2/5) and return flame region.
 
@@ -30,21 +30,17 @@ def flame_region(Q_dot_conv_kW: float, z: Union[float, np.ndarray]) -> Union[int
             return 1  # 'intermittent'
 
 
-def centre_line_temperature(Q_dot_kW: float, z: float, T_0: float, region: Optional[float] = None) -> (float, float):
+def fire_plume_temperature_rise_centreline(Q_dot_conv_kW: float, z: float, region: int, T_0: float = 293.15, ) -> float:
     """
     Calculate centre-line temperature of a fire plume.
 
-    :param Q_dot_kW: kW, total heat release rate.
+    :param Q_dot_conv_kW: kW, total heat release rate.
     :param z: m, height where the fire plume diameter is measured.
     :param T_0: K, initial temperature.
     :param region: 1, Optional, will be calculated if not provided. 0: flame, 1: intermittent and 2: plume
     :return: Tuple of temperature rise and region check value.
     """
-    z_Q_c_factor = z / (Q_dot_kW ** (2 / 5))
-    region = flame_region(Q_dot_kW, z) if region is None else region
-
     C = 0.9
-
     if isinstance(region, np.ndarray):
         k = np.where(region == 0, 6.8, np.where(region == 2, 1.1, np.where(region == 1, 1.9, np.nan)))
         nu = np.where(region == 0, 1 / 2, np.where(region == 2, -1 / 3, np.where(region == 1, 0, np.nan)))
@@ -57,13 +53,30 @@ def centre_line_temperature(Q_dot_kW: float, z: float, T_0: float, region: Optio
             k, nu = 1.9, 0
         else:
             raise ValueError(f'Unrecognised region {region}')
+    z_Q_c_factor = z / (Q_dot_conv_kW ** (2 / 5))
+    delta_T = (((k / C) ** 2) * (z_Q_c_factor ** ((2 * nu) - 1)) * T_0) / (2 * 9.81)
+    return delta_T
 
-    delta_T = ((k / C) ** 2 * z_Q_c_factor ** ((2 * nu) - 1) * T_0) / (2 * 9.81)
-    return delta_T, z_Q_c_factor
+
+def __plume_diameter(Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float, coeff: float, T_0: float = 293.15):
+    """
+    Calculate diameter of the plume at a certain height.
+    Equation 13.21 in Chapter 51, SFPE Handbook (2017)
+
+    :param Q_dot_kW: kW, total heat release rate.
+    :param Q_dot_dd_kW_m2: kW/m2, heat release rate per unit area.
+    :param z: m, height where the fire plume diameter is measured.
+    :return: Diameter of the plume.
+    """
+    r = (((Q_dot_kW / Q_dot_dd_kW_m2) / np.pi) ** 0.5)
+    z_0 = eq_10_virtual_origin(D=2 * r, Q_dot_kW=Q_dot_kW)
+    d = coeff * ((T_c / T_0) ** 0.5) * (z - z_0)
+    return d
 
 
 def plume_diameter_visible(
-        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float = None, T_0: float = 293.15) -> float:
+        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float, T_0: float = 293.15
+) -> float:
     """
     Calculate diameter of the plume at a certain height.
     Equation 51.54 in Chapter 51, SFPE Handbook (2017)
@@ -73,14 +86,12 @@ def plume_diameter_visible(
     :param z: m, height where the fire plume diameter is measured.
     :return: Diameter of the plume.
     """
-    T_c = centre_line_temperature(Q_dot_kW=Q_dot_kW, z=z, T_0=T_0) if T_c is None else T_c
-    z_0 = eq_10_virtual_origin(Q_dot_kW / Q_dot_dd_kW_m2, Q_dot_kW)
-    d = 0.48 * ((T_c - T_0) ** 0.5) * (z - z_0)
-    return d
+    return __plume_diameter(Q_dot_kW, Q_dot_dd_kW_m2, z, T_c, 0.48, T_0)
 
 
 def plume_diameter_at_50_temperature(
-        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float = None, T_0: float = 293.15) -> float:
+        Q_dot_kW: float, Q_dot_dd_kW_m2: float, z: float, T_c: float, T_0: float = 293.15
+) -> float:
     """
     Calculate diameter of the plume at a certain height.
     Equation 13.21 in Chapter 13, SFPE Handbook (2017)
@@ -90,7 +101,4 @@ def plume_diameter_at_50_temperature(
     :param z: m, height where the fire plume diameter is measured.
     :return: Diameter of the plume.
     """
-    T_c = centre_line_temperature(Q_dot_kW=Q_dot_kW, z=z, T_0=T_0) if T_c is None else T_c
-    z_0 = eq_10_virtual_origin(Q_dot_kW / Q_dot_dd_kW_m2, Q_dot_kW)
-    d = 0.12 * ((T_c - T_0) ** 0.5) * (z - z_0)
-    return d
+    return 2 * __plume_diameter(Q_dot_kW, Q_dot_dd_kW_m2, z, T_c, 0.12, T_0)
--- fsetools/lib/test_fse_plume.py
+++ fsetools/lib/test_fse_plume.py
@@ -2,33 +2,15 @@ from fsetools.lib.fse_plume import *
 
 
 def test_1():
-    verification_z = (1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,)
-    verification_delta_T = (852.514, 527.425, 351.617, 221.328, 152.588, 112.603, 87.091, 69.714, 57.288,)
-    verification_plume_diameter = (2.288, 3.227, 4.167, 5.106, 6.045, 6.984, 7.924, 8.863, 9.802,)
-
-    x1, y1, x2, y2 = list(), list(), list(), list()
-
-    for z_, delta_T_, fire_diameter_ in zip(verification_z, verification_delta_T, verification_plume_diameter):
-        delta_T, z_Q_c_factor = centre_line_temperature(Q_c_kW=1000, z=z_, T_0=293)
-        z_0 = virtual_origin(Q_c_kW=1000, HRRPUA_kW_m2=250, conv_frac=0.7)
-        plume_diameter__ = plume_diameter(Q_c_kW=1000, HRRPUA_kW_m2=250, conv_frac=0.7, z_0=z_0, z=z_)
-        assert abs(delta_T_ - delta_T) < 1e-3
-        assert abs(fire_diameter_ - plume_diameter__) < 1e-3
-        x1.append(z_Q_c_factor)
-        y1.append(delta_T)
-        x2.append(z_)
-        y2.append(plume_diameter__)
-
-    return x1, y1, x2, y2,
+    pass
 
 
 def test_2():
     import matplotlib.pyplot as plt
 
     z = np.linspace(0.1, 10, 100)
-    delta_T, z_Q_c_factor = centre_line_temperature(Q_c_kW=1000, z=z, T_0=293)
-    z_0 = virtual_origin(Q_c_kW=1000, HRRPUA_kW_m2=250, conv_frac=0.7)
-    plume_diameter__ = plume_diameter(Q_c_kW=1000, HRRPUA_kW_m2=250, conv_frac=0.7, z_0=z_0, z=z)
+    z_Q_c_factor = z / (1000 ** (2 / 5))
+    delta_T = fire_plume_temperature_rise_centreline(1000, z, fire_plume_region(1000, z))
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(5, 5))
 
@@ -39,11 +21,6 @@ def test_2():
     ax1.set_ylabel('dT [$^o$C]')
     ax1.set_xlabel('z/$Q_{c}$$^{2/5}$')
     ax1.grid()
-    ax2.plot(z, plume_diameter__)
-    ax2.set_ylabel('Plume diameter [m]')
-    ax2.set_xlabel('Height above ground [m]')
-    ax2.grid()
-    fig.tight_layout()
     fig.show()
 
 
